658438282b1dddeca4056de673c7a1f3ab783ca5
RAHAT11790 <rahatsarker224@gmail.com>
2025-10-09 12:40:23 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,8 +21,8 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 # CONFIGURATION
 # -----------------------
 API_ID = int(os.environ.get('API_ID', ''))
-API_HASH = os.environ.get('API_HASH', ''))
-BOT_TOKEN = os.environ.get('BOT_TOKEN', ''))
+API_HASH = os.environ.get('API_HASH', '')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 OWNER_ID = int(os.environ.get('OWNER_ID', ''))
 
 if not all([API_ID, API_HASH, BOT_TOKEN, OWNER_ID]):
