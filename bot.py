1cac6958a7442ac1a33222b31f550f79cb8601a3
RAHAT11790 <rahatsarker224@gmail.com>
2025-11-12 07:42:32 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ def keep_alive():
 # --- End Flask Keep Alive ---
 
 # --- Configuration ---
-TOKEN = '8272754175:AAHuLGphdeVjRoIMPJRAPz1yI006NgGYAZA'
+TOKEN = '8255788054:AAG9yzq6Q5L2CZWE6_z2mQzdFFXQhCewoRQ'
 OWNER_ID = 6621572366
 ADMIN_ID = 6621572366
 YOUR_USERNAME = '@RS_WONER'
